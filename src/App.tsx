b8acd3ae7d31831d71deeafd2124b0dd6fefa993
arigato mac16 <teeruch.k@gmail.com>
2024-08-09 09:14:37 +0700

    add API key

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { useEffect, useState } from "react";
 import "./App.css";
 import axios from "axios";
 
+const API_KEY = "asdfsadf";
+
 function App() {
   const [message, setMessage] = useState("");
   useEffect(() => {
@@ -16,6 +18,7 @@ function App() {
   return (
     <>
       <h1>Hello world</h1>
+      <p>{API_KEY}</p>
       <p>{message}</p>
     </>
   );
