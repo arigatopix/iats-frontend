1cf06aa99de238a51a38f1dcc992aa429e915a64
arigatopix <teeruch.kae@pea.co.th>
2024-09-18 12:03:47 +0700

    add footer in login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,27 +3,39 @@ import LoginForm from "../features/authentication/LoginForm";
 import Logo from "../ui/Logo";
 import Heading from "../ui/Heading";
 import AdminLogin from "../features/admin/AdminLogin";
+import Footer from "../ui/Footer";
 
 const LoginLayout = styled.main`
   min-height: 100vh;
   display: grid;
-  grid-template-columns: 48rem;
+  grid-template-columns: 52rem;
   align-content: center;
   justify-content: center;
   gap: 3.2rem;
   background-color: var(--color-grey-50);
 `;
 
+const WrapStyledFooter = styled(Footer)`
+  & {
+    background-color: var(--color-grey-50);
+    border-bottom: none;
+  }
+`;
+
 function Login() {
   return (
-    <LoginLayout>
-      <Logo />
+    <>
+      <LoginLayout>
+        <Logo />
 
-      <Heading as="h4">ระบบติดตามงานด้านวิเทศสัมพันธ์</Heading>
-      <LoginForm />
+        <Heading as="h4">ระบบติดตามงานด้านวิเทศสัมพันธ์</Heading>
+        <LoginForm />
 
-      {import.meta.env.VITE_APP_ENV !== "production" && <AdminLogin />}
-    </LoginLayout>
+        {import.meta.env.VITE_APP_ENV !== "production" && <AdminLogin />}
+
+        <WrapStyledFooter />
+      </LoginLayout>
+    </>
   );
 }
 
